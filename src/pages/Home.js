edf15f77f633dd101ec156dc3d7c0d6723ed3ef1
Josh8011 <64683297+Josh8011@users.noreply.github.com>
2022-04-27 11:00:05 +1000

    Added basic pages and router

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import {Header} from '../componenets';
+export function Home (){
+    return(
+        <div>
+        <Header/>    
+            Home Test
+        </div>
+    );
+}
