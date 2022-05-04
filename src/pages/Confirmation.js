ee184088f1ba9c2212bdca0f0f7eb5652caf5d9f
Atif <atifonat@gmail.com>
2022-05-04 15:13:12 +1000

    minor changes

--- src/pages/Confirmation.js
+++ src/pages/Confirmation.js
@@ -4,13 +4,17 @@ import { useLocation } from 'react-router-dom';
 
 export function Confirmation(){
     const location = useLocation();
-    console.log(location.state.newRes);
+    const reservation = location.state.newRes;
   
     return(
         <Container>
             <Row>
                 <Col sm={12}>
-                    RESERVATION CONFIRMED!
+                    <div className='mt-3'>
+                        <h1>Confirmation</h1>
+                        <br />
+                        Reference Number: {reservation.referenceNo}
+                    </div>
                 </Col>
             </Row>
         </Container>
