f78fb42841cf3eb59ca0ea0814cfd7d252c505f1
Mason-D <masonglendixon@gmail.com>
2022-05-05 09:50:50 +1000

    Cleaned up passed props to Router

--- src/Navigation/Router.js
+++ src/Navigation/Router.js
@@ -9,18 +9,16 @@ import { People, DatePage, Sitting, Details, Confirmation } from '../Pages'
 export function Router(props){
 
     var ResFunctions = props.ResFunctions
-    var ResInfo = props.ResInfo
-    var SelectPage = props.SelectPage
     var Sittings = props.Sittings
 
     return(
         <Routes>
-            <Route path='/' element={<People ResFunctions={ResFunctions} ResInfo={ResInfo}  SelectPage={SelectPage}/>}></Route>
-            <Route path='/People' element={<People ResFunctions={ResFunctions} ResInfo={ResInfo} SelectPage={SelectPage} />}></Route>
-            <Route path='/DatePage' element={<DatePage ResFunctions={ResFunctions} ResInfo={ResInfo} SelectPage={SelectPage} Sittings={Sittings} />}></Route>
-            <Route path='/Sitting' element={<Sitting ResFunctions={ResFunctions} ResInfo={ResInfo} SelectPage={SelectPage} />}></Route>
-            <Route path='/Details' element={<Details rResFunctions={ResFunctions} ResInfo={ResInfo} SelectPage={SelectPage} />}></Route>
-            <Route path='/Confirmation' element={<Confirmation />}></Route>
+            <Route path='/' element={<People ResFunctions={ResFunctions}/>}></Route>
+            <Route path='/People' element={<People ResFunctions={ResFunctions} />}></Route>
+            <Route path='/DatePage' element={<DatePage ResFunctions={ResFunctions} Sittings={Sittings} />}></Route>
+            <Route path='/Sitting' element={<Sitting ResFunctions={ResFunctions} />}></Route>
+            <Route path='/Details' element={<Details ResFunctions={ResFunctions} />}></Route>
+            <Route path='/Confirmation' element={<Confirmation ResFunctions={ResFunctions} />}></Route>
         </Routes>
     )
 }
