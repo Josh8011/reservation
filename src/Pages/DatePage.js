1022be4acdbf8b1a39eeaa47ad27cf31101f1457
Joshua Zimbicki <joshua.zimbicki@studytafensw.edu.au>
2022-05-05 10:09:01 +1000

    half deleted

--- src/Pages/DatePage.js
+++ src/Pages/DatePage.js
@@ -16,40 +16,10 @@ export function DatePage(props){
     // do we want this to defaul to the current month or do we want it to be selected.
     const [selectedMonth, setSelectedMonth] = useState(currentDate.getMonth()); 
     const [selectedMonthDates, setSelectedMonthDates] = useState([]);
-    //if Data
-    //Manage Data 
-    if(Sittings[0]){
-        var sittingsByMonth = SortSittingsByMonth(Sittings);
-        var distinctMonths = [...new Set(Sittings.map(x => x.month))];
-        var distinctDayByMonth = []
 
-        for(let i =0; i < distinctMonths.length; i++){
-            distinctDayByMonth.push([...new Set(sittingsByMonth[i].map(x => x.day))])
-        }
-        console.log(Sittings)
-        // console.log(distinctMonths)
-        // console.log(sittingsByMonth)
 
-        // console.log(distinctDayByMonth)
-    }
     
-    useEffect(()=>{
-        if(Sittings[0]){
-            let isDays = false;
-            for(let i=0; i< distinctMonths.length; i++)
-            {
-                if(selectedMonth == distinctMonths[i])
-                {
-                    setSelectedMonthDates(distinctDayByMonth[i])
-                    isDays = true;
-                }
-            }
-            if(!isDays)
-            {
-                setSelectedMonthDates(null)
-            }
-        }
-    },[selectedMonth])
+    
 
 
 
@@ -61,12 +31,7 @@ export function DatePage(props){
 
     return(
         <div className="DatePageContainer">
-            <div>
-            {monthBtns}
-            </div>
-            <div>
-            {<DateSelectionContainer SelectedMonth={selectedMonth} SelectedMonthDates={selectedMonthDates} ChangeDate={ChangeDate} SelectPage={SelectPage}/>}
-            </div>
+      
         </div>
     );
 }
@@ -86,29 +51,4 @@ function CreateMonthBtns(NumberOfBtns, currentDate, setSelectedMonth){
         currentDate.setMonth(currentDate.getMonth()+1)
     }
     return(MonthBtns)
-}
-
-function SortSittingsByMonth(Sittings){
-    var AllSittingsByMonth = []
-    var MonthSittings = []
-    var CurrentMonth = Sittings[0].month;
- 
-    for(let i = 0; i < Sittings.length; i++){
-        
-        if(Sittings[i].month == CurrentMonth)
-        {
-            MonthSittings.push(Sittings[i])
-        }
-        if(Sittings[i].month != CurrentMonth){
-            CurrentMonth = Sittings[i].month
-            AllSittingsByMonth.push(MonthSittings)
-            MonthSittings = []
-        }
-        if(i == Sittings.length-1)
-        {
-            AllSittingsByMonth.push(MonthSittings)
-        }           
-    }
-
-    return AllSittingsByMonth;
 }
