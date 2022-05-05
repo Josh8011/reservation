d2bfc6a471a5417151b128cd68427603d6d55b1e
Joshua Zimbicki <joshua.zimbicki@studytafensw.edu.au>
2022-05-05 12:21:46 +1000

    gutted compoenets

--- src/Components/DateSelectionBtn.js
+++ src/Components/DateSelectionBtn.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import './Css/DateSelectionBtn.css'
-import { useNavigate } from 'react-router-dom';
+
 
 export function DateSelectionBtn(props){
-    var SelectedMonth = props.SelectedMonth
-    var number = props.number;
-    var ChangeDate = props.ChangeDate
-    var SelectPage = props.SelectPage
-    let navigate = useNavigate();
-
-    function DateSelectionOnClick(){
-        ChangeDate(`${number}/${SelectedMonth}`);
-        SelectPage("Sitting");
-        navigate('/sitting')
-    }
+ 
 
     return(
-        <div className="DateSelectionBtn" onClick={DateSelectionOnClick} to={"/Sitting"} >{number}</div>
+        <div className="DateSelectionBtn" >
+
+        </div>
     );
 }
