4a6663c28d6d0c921c8f3d948d44a800210296e5
Mason-D <masonglendixon@gmail.com>
2022-05-17 13:20:32 +1000

    icons

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { HeaderLink } from './HeaderLink';
 import './Css/Header.css';
+import people from './Icons/people.png'
 
 
 export function Header(props) {
@@ -15,13 +16,19 @@ export function Header(props) {
 
 
     return(
-        <div className='Body'>
+        <div className='container'>
+            <div className='Body'>
+                
+                <HeaderLink selected={selected} Name={ResInfo.people? ResInfo.people: "People" } Path="people" ResInfo={ResInfo}/>
+                <img src={people} className="image" />
+                <HeaderLink selected={selected} Name={ResInfo.date?
+                    `${addPadding(ResInfo.date.day)}/${addPadding(ResInfo.date.month)}/${ResInfo.date.year}`: 'Date'} Path="date" ResInfo={ResInfo}/>
+                <HeaderLink selected={selected} Name={ResInfo.sitting? ResInfo.sitting.Start :"Sitting"} Path="sitting" ResInfo={ResInfo}/>
+                <HeaderLink selected={selected} Name="Details" Path="details" ResInfo={ResInfo}/>       
+
+            </div>
             
-            <HeaderLink selected={selected} Name={ResInfo.people? ResInfo.people: "People" } Path="people" ResInfo={ResInfo}/>
-            <HeaderLink selected={selected} Name={ResInfo.date?
-                `${addPadding(ResInfo.date.day)}/${addPadding(ResInfo.date.month)}/${ResInfo.date.year}`: 'Date'} Path="date" ResInfo={ResInfo}/>
-            <HeaderLink selected={selected} Name={ResInfo.sitting? ResInfo.sitting.Start :"Sitting"} Path="sitting" ResInfo={ResInfo}/>
-            <HeaderLink selected={selected} Name="Details" Path="details" ResInfo={ResInfo}/>       
+
 
         </div>
     );
